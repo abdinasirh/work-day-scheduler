1208ff0dff021d629efc4cd60331fa8c40451d2a
Abdinasir Hassan <abdinasirduud20@gmail.com>
2021-11-14 23:52:05 -0600

    modified the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
-//display current day & time.
-var date = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").text(date);
+//display current date & time.
+var currrentDate = moment().format('MMMM Do YYYY, h:mm');
+$("#currentDay").text(currrentDate);
 
+// color coded time block to determine if its in the past, present or future
+var timeTracker = function() {
+    var hour = moment().hours();
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id"));
+
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+    
+
+    
+};
+timeTracker();
